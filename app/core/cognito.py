13895cab2ab58f9722df97800ea478ba9087fb53
NonymousMorlock <agamspaul@gmail.com>
2025-10-07 19:28:35 +0000

    ✨ feat(cognito): Implement function to get Cognito client

---
+++ app/core/cognito.py
@@ -0,0 +1,8 @@
+import boto3
+from botocore.client import BaseClient
+
+from app.core.config import settings
+
+
+def get_cognito_client() -> BaseClient:
+    return boto3.client('cognito-idp', region_name=settings.REGION_NAME)
