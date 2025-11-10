9a25b7622122638ec93b9bab74e71a3cb5bfbb37
NonymousMorlock <agamspaul@gmail.com>
2025-11-10 21:36:03 +0000

    🐛 fix(database): Remove unnecessary `connect_args` from database engine creation

--- app/core/database.py
+++ app/core/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from .config import settings
 
-engine = create_engine(settings.POSTGRES_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(settings.POSTGRES_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
