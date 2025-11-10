2f5180680c3efd6f131ec2f61c94bfb5cbf4731b
NonymousMorlock <agamspaul@gmail.com>
2025-11-10 21:34:11 +0000

    ✅ build(config): Add AWS and Redis configuration options to settings

--- app/core/config.py
+++ app/core/config.py
@@ -15,6 +15,17 @@ class Settings(BaseSettings):
 
     # AWS
     REGION_NAME: str
+    # AWS_ACCESS_KEY_ID: str
+    # AWS_SECRET_ACCESS_KEY: str
+
+    # S3
+    S3_RAW_VIDEOS_BUCKET: str
+    S3_PROCESSED_VIDEOS_BUCKET: str
+    S3_VIDEO_THUMBNAILS_BUCKET: str
+
+    # Redis
+    REDIS_HOST: str
+    REDIS_PORT: int
 
 
 settings = Settings()
